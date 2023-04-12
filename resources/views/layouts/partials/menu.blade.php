956f58d806e84bcf1b0821eb4abf5484ddd20a76
Ahmad Ilham <ilham@sociabuzz.com>
2023-04-12 16:15:13 +0700

    hide affileasi

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -66,12 +66,12 @@
                             </li>
                         @endif
                     @endforeach
-                    <li class="menu-item {{ isset($active) && $active == 'referral' ? 'active' : '' }}">
+                    {{-- <li class="menu-item {{ isset($active) && $active == 'referral' ? 'active' : '' }}">
                         <a href="{{ route('users.referral') }}" class="menu-link">
                             <i class="menu-icon tf-icons mdi mdi-share-all-outline"></i>
                             <div data-i18n="Affiliasi">Affiliasi</div>
                         </a>
-                    </li>
+                    </li> --}}
                 </ul>
             </li>
 
@@ -130,12 +130,12 @@
                         </a>
                     </li>
 
-                    <li class="menu-item {{ isset($active) && $active == 'komisi' ? 'active' : '' }}">
+                    {{-- <li class="menu-item {{ isset($active) && $active == 'komisi' ? 'active' : '' }}">
                         <a href="{{ route('deposito.list_affilate') }}" class="menu-link">
                             <i class="menu-icon tf-icons mdi mdi-share-all-outline"></i>
                             <div data-i18n="Komisi Affiliasi">Komisi Affiliasi</div>
                         </a>
-                    </li>
+                    </li> --}}
                 </ul>
             </li>
 
