1f93d6c6a0e8b0fb8021ffbd8c2bb22874ef099b
Ahmad Ilham <ilham@sociabuzz.com>
2023-04-17 08:41:59 +0700

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,8 +33,8 @@ class HomeController extends Controller
         if($user->hasRole('member')){
             $types = DepositoType::get();
             $deposito = $user->deposito()->whereIn('status',[1,4])->sum('amount');
-            // $profit = $user->deposito()->whereIn('status',[1,4])->sum('profit');
-            $profit = $user->profit()->sum('amount');
+            $profit = $user->deposito()->whereIn('status',[1,4])->sum('profit');
+            // $profit = $user->profit()->sum('amount');
             $withdraw = $user->withdrawal()->where('status',1)->sum('amount');
             return view('home', compact('types','deposito','profit','withdraw'));
         }else{
